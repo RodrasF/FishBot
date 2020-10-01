8acd7a9375650a6e6fea830c44f0c095ad32d670
Rodrigo Frazão <41315902+RodrasF@users.noreply.github.com>
2020-10-01 03:08:20 +0100

    Need to commit

--- bot.js
+++ bot.js
@@ -1,48 +1,77 @@
-require('dotenv').config();
 const tmi = require('tmi.js');
+const tps = require('twitchps');
 
-// Define configuration options
-const opts = {
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN
-  },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
-};
-// Create a client with our options
-const client = new tmi.client(opts);
+module.exports = function (accessToken, channel) {
+  // Define TMI configuration options
+  const opts = {
+    options: {
+      debug: true
+    },
+    connection: {
+      secure: true,
+      reconnect: true
+    },
+    identity: {
+      username: process.env.BOT_USERNAME,
+      password: process.env.BOT_OAUTH_TOKEN
+    },
+    channels: [
+      channel.login
+    ]
+  };
 
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+  // Create a TMI client with our options
+  const client = new tmi.client(opts);
 
-// Connect to Twitch:
-client.connect();
+  // Register our TMI event handlers (defined below)
+  client.on('message', onMessageHandler);
+  client.on('connected', onConnectedHandler);
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+  // Initialize topics for PubSub
+  const init_topics = [{
+    topic: `channel-points-channel-v1.${channel.id}`,
+    token: accessToken,
+  }];
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+  // Create a PubSub client with our options
+  const pubSub = new tps({
+    init_topics,
+    reconnect: true,
+    debug: true,
+  });
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+  // Register our event handlers for PubSub
+  pubSub.on('channel-points', onRewardHandler);
+
+  // Connect to Twitch:
+  client.connect();
+
+  // Called every time a message comes in
+  function onMessageHandler (target, context, msg, self) {
+    if (self) { return; } // Ignore messages from the bot
+
+    // Remove whitespace from chat message
+    const commandName = msg.trim();
+
+    // If the command is known, let's execute it
+    if (commandName) {
+      console.log(`* Executed ${commandName} command`);
+    } else {
+      console.log(`* Unknown command ${commandName}`);
+    }
   }
-}
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+
+  // Called every time the bot connects to Twitch chat
+  function onConnectedHandler (addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+  }
+
+  async function onRewardHandler (data) {
+    try {
+      const { redemption } = data;
+      
+    } catch (error) {
+      console.log('* Error handling reward redemption', data, error);
+    }
+  }
 }
---
+++ index.js
@@ -0,0 +1,91 @@
+require('dotenv').config();
+const express        = require('express');
+const session        = require('express-session');
+const passport       = require('passport');
+const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+const request        = require('request');
+const bot            = require('./bot');
+
+const CALLBACK_URL = `http://localhost:${process.env.PORT}/auth/twitch/callback`;
+
+const app = express();
+app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false}));
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Override passport profile function to get user profile from Twitch API
+OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
+  var options = {
+    url: 'https://api.twitch.tv/helix/users',
+    method: 'GET',
+    headers: {
+      'Client-ID': process.env.CLIENT_ID,
+      'Accept': 'application/vnd.twitchtv.v5+json',
+      'Authorization': 'Bearer ' + accessToken
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (response && response.statusCode == 200) {
+      done(null, JSON.parse(body));
+    } else {
+      done(JSON.parse(body));
+    }
+  });
+}
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use('twitch', new OAuth2Strategy({
+    authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+    tokenURL: 'https://id.twitch.tv/oauth2/token',
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: CALLBACK_URL,
+    scope: 'channel:read:redemptions',
+    state: true
+  },
+  function(accessToken, refreshToken, profile, done) {
+    profile.accessToken = accessToken;
+    profile.refreshToken = refreshToken;
+
+    // Securely store user profile in your DB
+    //User.findOrCreate(..., function(err, user) {
+    //  done(err, user);
+    //});
+    done(null, profile);
+  }
+));
+
+// Set route to start OAuth link, this is where you define scopes to request
+app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'user_read' }));
+
+// Set route for OAuth redirect
+app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
+
+
+// If user has an authenticated session, display it, otherwise display link to authenticate
+app.get('/', function (req, res) {
+  if(req.session && req.session.passport && req.session.passport.user) {
+    res.json(req.session.passport.user);
+    const {accessToken} = req.session.passport.user
+    const channel = req.session.passport.user.data[0];
+    bot(accessToken, channel);
+  } else {
+    res.redirect('/auth/twitch');
+  }
+});
+
+app.use('/', router);
+  
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('* Listening on port', port);
+});
